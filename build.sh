67e993c40eb369a8d18dd1031a8ef06e6e70a3bd
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-07-11 09:52:41 -0500

    update

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fsl_version="6.0.7.18"
-docker_user="baxterprogers"
-docker_tag="${docker_user}/fsl-base:v${fsl_version}"
-
-docker build --build-arg FSLVER="${fsl_version}"  -t "${docker_tag}" .
